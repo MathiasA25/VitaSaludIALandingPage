02bfb2d3d4a54265005a7d6b83401e61f3b04594
MathiasA25 <u202314898@upc.edu.pe>
2025-06-27 19:45:56 -0500

    Ultima Actualizacion

--- Landing Page-public/assets/scripts/script.js
+++ Landing Page-public/assets/scripts/script.js
@@ -1,19 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const botonEscucha = document.getElementById("btn-escucha");
-  
-    botonEscucha?.addEventListener("click", () => {
-      alert("Funcionalidad de voz aún en desarrollo.");
-    });
-  
-    const formularios = document.querySelectorAll("form");
-  
-    formularios.forEach(form => {
-      form.addEventListener("submit", e => {
-        e.preventDefault();
-        alert("Gracias por tu mensaje. Recibido correctamente.");
-      });
+  const botonEscucha = document.getElementById("btn-escucha");
+
+  botonEscucha?.addEventListener("click", () => {
+    alert("Funcionalidad de voz aún en desarrollo.");
+  });
+
+  const formularios = document.querySelectorAll("form");
+
+  formularios.forEach(form => {
+    form.addEventListener("submit", e => {
+      e.preventDefault();
+      alert("Gracias por tu mensaje. Recibido correctamente.");
     });
   });
-  
+});
 
   
